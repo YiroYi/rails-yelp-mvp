b1da63413ef9344c866991b3f5cf28835c217d87
YiroYi <yiroyi@live.com>
2020-05-13 12:24:59 +0900

    Challenge 02 : Yelp | Database seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all if Rails.env.development?
+
+category = ['chinese', 'italian', 'japanese','french', 'belgian']
+
+5.times do
+  Restaurant.create(name: Faker::Beer.name,
+                    address: Faker::Address.city,
+                    phone_number: Faker::PhoneNumber.cell_phone,
+                    category: category.sample)
+end
